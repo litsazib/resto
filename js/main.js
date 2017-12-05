4c27f678f087ac1f754e617589b212a26dce21e4
litsazib <roktimsazib92@gmail.com>
2017-12-05 15:51:13 +0600

    food menu

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     $(".item").mouseover(function(){
+//           $("#overlay").addClass('overlay');
+//     });
+//     $(".item").mouseout(function(){
+//         $("#overlay").removeClass('overlay');
+//     });
+// 
+// 
+// });
